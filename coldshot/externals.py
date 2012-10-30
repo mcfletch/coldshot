641d607068d30e5c2570854a25c6d55f6d59480c
Mike C. Fletcher <mcfletch@vrplumber.com>
2012-10-30 11:39:42 -0400

    Make the command-line a bit more useful and forgiving

--- coldshot/externals.py
+++ coldshot/externals.py
@@ -1,10 +1,11 @@
 """Top level (mainloop-like) operations"""
 from . import profiler, loader, reporter
 import tempfile, atexit, sys
+from optparse import OptionParser
 
 __all__ = ('run','runctx')
 
-def run( code, filename=None ):
+def run( code, filename=None, lines=False ):
     """Run exec-able code under the profiler
     
     code -- exec-able code (string, code, file) to run 
@@ -16,9 +17,9 @@ def run( code, filename=None ):
     
     returns profiler.Profiler instance
     """
-    return runctx( code, {}, {}, filename )
+    return runctx( code, {}, {}, filename, lines=lines )
     
-def runctx( code, globals=None, locals=None, prof_dir=None ):
+def runctx( code, globals=None, locals=None, prof_dir=None, lines=False ):
     """Run exec-able code under the profiler
     
     code -- exec-able code (string, code, file) to run 
@@ -44,7 +45,7 @@ def runctx( code, globals=None, locals=None, prof_dir=None ):
         globals = {}
     if locals is None:
         locals = globals 
-    prof = profiler.Profiler( prof_dir )
+    prof = profiler.Profiler( prof_dir, lines=lines )
     atexit.register( prof.stop )
     prof.start()
     try:
@@ -53,23 +54,42 @@ def runctx( code, globals=None, locals=None, prof_dir=None ):
         prof.stop()
     return prof
 
-def profile():
+def profile_options( ):
+    """Create an option parser for the main profile operation"""
+    usage = "coldshot -o output_directory path/to/scriptfile [arg] ..."
+    parser = OptionParser( usage )
+    parser.add_option( 
+        '-o', '--output', dest='output', metavar='DIRECTORY', default='.profile',
+        help='Directory to which to write output (file index.profile will be created here)',
+    )
+    parser.add_option(
+        '-l', '--lines', dest='lines',
+        action = 'store_true',
+        default = False,
+        help='Perform line-level tracing (requires an extra 2.5MB/s of disk space)',
+    )
+    # 
+    parser.disable_interspersed_args()
+    return parser
+    
+def profile(args=None):
     """Primary external entry point for profiling 
     
-    $ coldshot <outdirectory> scriptfile [arg ... ]
+    $ coldshot -o outdirectory scriptfile [arg ... ]
     
     Note: this command line is currently too simplistic, as it can wind up 
     mis-interpreting the arguments if you happen to forget the outdirectory.
     """
     import os, sys
-    from optparse import OptionParser
-    usage = "coldshot output_directory scriptfile [arg] ...\n"
-    try:
-        output,scriptfile = sys.argv[1:3]
-    except (TypeError,ValueError) as err:
-        sys.stderr.write( usage )
-        return 2
-    sys.argv[:] = [scriptfile] + sys.argv[3:]
+    parser = profile_options()
+    options,args = parser.parse_args(args or sys.argv)
+    args = args[1:]
+    
+    if not args:
+        parser.error( "Need a script-file to execute" )
+        return 1
+    scriptfile = args[0]
+    sys.argv[:] = args
     sys.path.insert(0, os.path.dirname(scriptfile))
     with open(scriptfile, 'rb') as fp:
         code = compile(fp.read(), scriptfile, 'exec')
@@ -78,8 +98,18 @@ def profile():
         '__name__': '__main__',
         '__package__': None,
     }
-    runctx(code, globals, None, output)
+    runctx(code, globals, None, prof_dir=options.output, lines=options.lines)
     return 0
+
+def report_options( ):
+    """Create an option parser for the main profile operation"""
+    usage = "coldshot-report [options] output_directory\n"
+    parser = OptionParser( usage )
+    parser.add_option( 
+        '-o', '--output', dest='output', metavar='DIRECTORY', default='.profile',
+        help='Directory to which to write output (file index.profile will be created here)',
+    )
+    return parser
 
 def report():
     """Load the data-set and print a basic report"""
