5738159f84e16078efbc58bf07248172975df3b5
Mike C. Fletcher <mcfletch@vrplumber.com>
2012-11-08 09:50:08 -0500

    Test the coldshot version instead of the cprofile version

--- tests/generating.py
+++ tests/generating.py
@@ -1,6 +1,7 @@
-from coldshot.cprofdecorator import profile
+from coldshot import profile
+import pdb 
+pdb.set_trace()
 
-@profile
 def a():
     for i in range( 8 ):
         for j in b():
@@ -9,6 +10,10 @@ def b():
     for i in range( 8 ):
         yield i**2
 
-for x in a():
-    print x
+@profile( 'generating.profile' )
+def main():
+    for x in a():
+        print x
     
+if __name__ == "__main__":
+    main()
